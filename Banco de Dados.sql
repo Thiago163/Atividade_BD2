302788c7273bc4f77deffd8a582faf4af97618c6
mistickesterio <thiagosilvadacruz163@gmail.com>
2022-09-17 16:50:36 -0300

    ghahjg

--- Banco de Dados.sql
+++ Banco de Dados.sql
@@ -28,7 +28,6 @@ CREATE TABLE Atendimento(
 INSERT INTO Cliente (Nome,CPF) VALUES
 ('Lucas', '12312312312'),
 ('Luiz', '32132132132'),
-('Maiara', '0654654464'),
 ('Lourival', '12312312312'),
 ('Otavio', '32132132132'),
 ('Kerolaine', '32132132132'),
@@ -39,15 +38,17 @@ INSERT INTO Produto (Nome,ID) VALUES
 ('Docê de Leite', '02'),
 ('Manteiga', '03'),
 ('Pastél', '04'),
-('Hot dog', '05'),
+('Coca Cola', '05'),
 ('Cachorro quente', '06');
 
 INSERT INTO Atendimento (Local, Data, ClienteID, IDProduto)VALUES
-('Supermercado', '2022-05-25 08:30:05','02', '01'),
-('Padaria', '2022-05-27 10:00:05','01', '03'),
-('Restaurante', '2022-06-15 13:34:05','02', '02'),
-('Lanchonete', '2022-07-01 07:30:05','01', '04'),
-('Praça de alimentação', '2022-08-14 14:30:05','04', '06'),
+('Supermercado', '2022-05-25 08:30:05','01', '01'),
+('Padaria', '2022-05-27 10:00:05','02', '02'),
+('Restaurante', '2022-06-15 13:34:05','03', '03'),
+('Lanchonete', '2022-07-01 07:30:05','04', '04'),
+('Praça de alimentação', '2022-08-14 14:30:05','05', '05'),
+('Praça de alimentação', '2022-08-14 14:30:05','06', '06'),
+('Praça de alimentação', '2022-08-14 14:30:05','03', '02'),
 ('Feira de Santana', '2022-09-12 15:30:05','05', '01');
 
 SELECT * FROM Atendimento
